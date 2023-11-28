e3ff03e4616dc1fc24571e53cebf2f2d5df57eec
Riccardi.20248 <Riccardi.20248@LABI2-13PC15>
2023-11-28 10:51:38 +0100

    Scrivere le classi quadrato e cubo sfruttando l'ereditarietà. Per ogni classe scrivere: - attributi - costruttori - metodi  get e set - metodi specifici ( per il quadrato calcolo dell'area  e del perimetro, per il cubo superficie totale e volume)
    * inserire provvisoriamente delle stampe all'interno dei costruttori per comprenderne la dinamica

--- 4 -  Prova ereditarietà/Program.cs
+++ 4 -  Prova ereditarietà/Program.cs
@@ -1,15 +1,60 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace _4____Prova_ereditarietà
 {
-    internal class Program
+    internal class program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Inserisci la lunghezza del lato");
+            double latoo = double.Parse(Console.ReadLine());
+            Quadrato quadrato = new Quadrato(latoo);
+            Cubo cubo = new Cubo(latoo);
+            quadrato.perimetro();
+            quadrato.area();
+            cubo.superficie();
+            cubo.volume();
+
         }
     }
-}
+    class Quadrato
+    {
+        protected double _lato;
+        public Quadrato(double lato)
+        {
+            this._lato = lato;
+        }
+        public void perimetro()
+        {
+            Console.WriteLine("Il perimetro è di: " + _lato * 4);
+        }
+        public void area()
+        {
+            Console.WriteLine("L'area è di: " + _lato * _lato);
+        }
+    }
+    class Cubo : Quadrato
+    {
+        public Cubo(double lato) : base(lato)
+        {
+            _lato = lato;
+        }
+        public void superficie () 
+        {
+            Console.WriteLine("La superficie è di: " + _lato * _lato * 6);
+        }
+        public void volume()
+        {
+            Console.WriteLine("Il volume è di: " + _lato * _lato * _lato);
+        }
+    }
+}        
+
+
+
+
